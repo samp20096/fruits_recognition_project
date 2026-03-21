7502e01650876f25081a0c052f96c99a71636d4b
Tal <samp20096@gmail.com>
2026-03-21 19:36:25 +0200

    Finish product of Fruits 360 Prediction

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# fruits_recognition_project
+# Fruits-360 Recognition App
+
+A Streamlit-based web application for classifying fruits and vegetables using a custom deep learning model trained on the Fruits-360 dataset. You can upload an image from your device or use your device's camera to take a picture and identify the fruit from 250 different classes.
+
+## Prerequisites
+
+Make sure you have Python installed (preferably Python 3.8 to 3.11). 
+
+## Setup Instructions
+
+To get this application running locally, please follow these steps carefully:
+
+### 1. Clone the repository
+
+First, clone this repository to your local machine and navigate into the project folder.
+
+```bash
+git clone <your-repository-url>
+cd fruits360_project
+```
+*(Note: Replace `<your-repository-url>` with the actual URL of your repository)*
+
+### 2. Install Dependencies
+
+It is recommended to use a virtual environment. Install the required libraries using pip:
+
+```bash
+pip install streamlit tensorflow numpy Pillow
+```
+
+### 3. Download the Model
+
+The app requires the pre-trained model file to function.
+
+1. Download the `fruits_360.keras` model file.
+2. Place the `fruits_360.keras` file directly in the same folder as `app.py` (the root of the project). 
+
+If the model is missing, the application will display an error message and refuse to run.
+
+### 4. Run the Application
+
+Once the repository is cloned, dependencies are installed, and the model is placed in the correct directory, you can launch the app:
+
+```bash
+streamlit run app.py
+```
+
+This will start a local server, and your default web browser will automatically open the app. If it doesn't open automatically, you can navigate to the URL provided in the terminal (usually `http://localhost:8501`).
+
+## How to use
+
+1. Select either to **Take a picture** using your camera or **upload from gallery**.
+2. Click **Save image to computer** if you wish to save a local copy of the picture.
+3. Click the **Fruit Recognition** button.
+4. The model will analyze the 100x100 resized version of the image and predict the fruit class!
--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-MODEL_PATH = "fruits_360.keras"
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(SCRIPT_DIR, "fruits_360.keras")
 
 if not os.path.exists(MODEL_PATH):
     """
@@ -90,7 +91,68 @@ if chosen is not None:
 
         prediction = model.predict(img)
         predicted_class = np.argmax(prediction[0])
-        predicted_label = model.classes_[predicted_class]
+        labels = {
+            0: 'Almonds 1', 1: 'Apple 10', 2: 'Apple 11', 3: 'Apple 12', 4: 'Apple 13', 
+            5: 'Apple 14', 6: 'Apple 17', 7: 'Apple 18', 8: 'Apple 19', 9: 'Apple 5', 
+            10: 'Apple 6', 11: 'Apple 7', 12: 'Apple 8', 13: 'Apple 9', 14: 'Apple Braeburn 1', 
+            15: 'Apple Core 1', 16: 'Apple Crimson Snow 1', 17: 'Apple Golden 1', 18: 'Apple Golden 2', 
+            19: 'Apple Golden 3', 20: 'Apple Granny Smith 1', 21: 'Apple Pink Lady 1', 22: 'Apple Red 1', 
+            23: 'Apple Red 2', 24: 'Apple Red 3', 25: 'Apple Red Delicious 1', 26: 'Apple Red Yellow 1', 
+            27: 'Apple Red Yellow 2', 28: 'Apple Rotten 1', 29: 'Apple hit 1', 30: 'Apple worm 1', 
+            31: 'Apricot 1', 32: 'Avocado 1', 33: 'Avocado Black 1', 34: 'Avocado Black 2', 
+            35: 'Avocado Green 1', 36: 'Avocado ripe 1', 37: 'Banana 1', 38: 'Banana 3', 
+            39: 'Banana 4', 40: 'Banana Lady Finger 1', 41: 'Banana Red 1', 42: 'Beans 1', 
+            43: 'Beetroot 1', 44: 'BlackBerry 3', 45: 'Blackberrie 1', 46: 'Blackberrie 2', 
+            47: 'Blackberrie half rippen 1', 48: 'Blackberrie not rippen 1', 49: 'Blueberry 1', 
+            50: 'Cabbage red 1', 51: 'Cabbage white 1', 52: 'Cactus fruit 1', 53: 'Cactus fruit green 1', 
+            54: 'Cactus fruit red 1', 55: 'Caju seed 1', 56: 'Cantaloupe 1', 57: 'Cantaloupe 2', 
+            58: 'Cantaloupe 3', 59: 'Carambula 1', 60: 'Carrot 1', 61: 'Cauliflower 1', 
+            62: 'Cherimoya 1', 63: 'Cherry 1', 64: 'Cherry 2', 65: 'Cherry 3', 66: 'Cherry 4', 
+            67: 'Cherry 5', 68: 'Cherry Rainier 1', 69: 'Cherry Rainier 2', 70: 'Cherry Rainier 3', 
+            71: 'Cherry Sour 1', 72: 'Cherry Wax Black 1', 73: 'Cherry Wax Red 1', 74: 'Cherry Wax Red 2', 
+            75: 'Cherry Wax Red 3', 76: 'Cherry Wax Yellow 1', 77: 'Cherry Wax not ripen 1', 
+            78: 'Cherry Wax not ripen 2', 79: 'Chestnut 1', 80: 'Clementine 1', 81: 'Cocos 1', 
+            82: 'Corn 1', 83: 'Corn Husk 1', 84: 'Cucumber 1', 85: 'Cucumber 10', 86: 'Cucumber 11', 
+            87: 'Cucumber 3', 88: 'Cucumber 4', 89: 'Cucumber 5', 90: 'Cucumber 6', 91: 'Cucumber 7', 
+            92: 'Cucumber 8', 93: 'Cucumber 9', 94: 'Cucumber Ripe 1', 95: 'Cucumber Ripe 2', 
+            96: 'Dates 1', 97: 'Eggplant 1', 98: 'Eggplant long 1', 99: 'Fig 1', 100: 'Ginger 2', 
+            101: 'Ginger Root 1', 102: 'Gooseberry 1', 103: 'Granadilla 1', 104: 'Grape Blue 1', 
+            105: 'Grape Pink 1', 106: 'Grape White 1', 107: 'Grape White 2', 108: 'Grape White 3', 
+            109: 'Grape White 4', 110: 'Grape not ripen 1', 111: 'Grapefruit Pink 1', 112: 'Grapefruit White 1', 
+            113: 'Guava 1', 114: 'Hazelnut 1', 115: 'Huckleberry 1', 116: 'Kaki 1', 117: 'Kiwi 1', 
+            118: 'Kohlrabi 1', 119: 'Kumquats 1', 120: 'Lemon 1', 121: 'Lemon Meyer 1', 122: 'Limes 1', 
+            123: 'Lychee 1', 124: 'Mandarine 1', 125: 'Mango 1', 126: 'Mango Red 1', 127: 'Mangostan 1', 
+            128: 'Maracuja 1', 129: 'Melon Piel de Sapo 1', 130: 'Mulberry 1', 131: 'Nectarine 1', 
+            132: 'Nectarine Flat 1', 133: 'Nectarine Flat 2', 134: 'Nut 1', 135: 'Nut 2', 136: 'Nut 3', 
+            137: 'Nut 4', 138: 'Nut 5', 139: 'Nut Forest 1', 140: 'Nut Pecan 1', 141: 'Onion 2', 
+            142: 'Onion Red 1', 143: 'Onion Red 2', 144: 'Onion Red Peeled 1', 145: 'Onion White 1', 
+            146: 'Onion White Peeled 1', 147: 'Orange 1', 148: 'Papaya 1', 149: 'Papaya 2', 
+            150: 'Passion Fruit 1', 151: 'Peach 1', 152: 'Peach 2', 153: 'Peach 3', 154: 'Peach 4', 
+            155: 'Peach 5', 156: 'Peach 6', 157: 'Peach Flat 1', 158: 'Peanut shell 1x 1', 
+            159: 'Pear 1', 160: 'Pear 10', 161: 'Pear 11', 162: 'Pear 12', 163: 'Pear 13', 
+            164: 'Pear 2', 165: 'Pear 3', 166: 'Pear 5', 167: 'Pear 6', 168: 'Pear 7', 
+            169: 'Pear 8', 170: 'Pear 9', 171: 'Pear Abate 1', 172: 'Pear Forelle 1', 173: 'Pear Kaiser 1', 
+            174: 'Pear Monster 1', 175: 'Pear Red 1', 176: 'Pear Stone 1', 177: 'Pear Williams 1', 
+            178: 'Pear common 1', 179: 'Pepino 1', 180: 'Pepper 2', 181: 'Pepper Green 1', 
+            182: 'Pepper Orange 1', 183: 'Pepper Orange 2', 184: 'Pepper Red 1', 185: 'Pepper Red 2', 
+            186: 'Pepper Red 3', 187: 'Pepper Red 4', 188: 'Pepper Red 5', 189: 'Pepper Yellow 1', 
+            190: 'Physalis 1', 191: 'Physalis with Husk 1', 192: 'Pineapple 1', 193: 'Pineapple Mini 1', 
+            194: 'Pistachio 1', 195: 'Pitahaya Red 1', 196: 'Plum 1', 197: 'Plum 2', 198: 'Plum 3', 
+            199: 'Plum 4', 200: 'Plum hole 1', 201: 'Pomegranate 1', 202: 'Pomelo Sweetie 1', 
+            203: 'Potato Red 1', 204: 'Potato Red Washed 1', 205: 'Potato Sweet 1', 206: 'Potato White 1', 
+            207: 'Quince 1', 208: 'Quince 2', 209: 'Quince 3', 210: 'Quince 4', 211: 'Rambutan 1', 
+            212: 'Raspberry 1', 213: 'Raspberry 2', 214: 'Raspberry 3', 215: 'Raspberry 4', 
+            216: 'Raspberry 5', 217: 'Raspberry 6', 218: 'Redcurrant 1', 219: 'Salak 1', 
+            220: 'Strawberry 1', 221: 'Strawberry 2', 222: 'Strawberry 3', 223: 'Strawberry Wedge 1', 
+            224: 'Tamarillo 1', 225: 'Tangelo 1', 226: 'Tomato 1', 227: 'Tomato 10', 228: 'Tomato 2', 
+            229: 'Tomato 3', 230: 'Tomato 4', 231: 'Tomato 5', 232: 'Tomato 7', 233: 'Tomato 8', 
+            234: 'Tomato 9', 235: 'Tomato Cherry Maroon 1', 236: 'Tomato Cherry Orange 1', 
+            237: 'Tomato Cherry Red 1', 238: 'Tomato Cherry Red 2', 239: 'Tomato Cherry Yellow 1', 
+            240: 'Tomato Heart 1', 241: 'Tomato Maroon 1', 242: 'Tomato Maroon 2', 243: 'Tomato Yellow 1', 
+            244: 'Tomato not Ripen 1', 245: 'Walnut 1', 246: 'Watermelon 1', 247: 'Zucchini 1', 
+            248: 'Zucchini Green 1', 249: 'Zucchini dark 1'
+        }
+        predicted_label = labels[predicted_class]
 
         st.write(f"Predicted Fruit: {predicted_label}")
 
